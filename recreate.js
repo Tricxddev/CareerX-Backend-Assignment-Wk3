2df805bc4af32c5b6fce36a9e6b337816cefdeda
Tricxddev <gpittaz67@gmail.com>
2024-07-23 13:22:17 +0100

    Add files via upload

---
+++ recreate.js
@@ -0,0 +1,36 @@
+
+
+// STRINGS 
+var FirstName = "GABRIEL"
+var LastName = "PETERS"
+var Jobtitle = "IT OFFICER"
+var Age ="30"
+var StateofOrigin ="OGUN STATE"
+
+// NUMBERS 
+var creditcddetails = 1000000009
+var Bankdetails = 2390878762
+var NumofCars   = 50
+var Numofsiblings   = 1
+var Formnum     = 56
+
+// BOOLEAN 
+var ageisgreaterthan18 = true
+var Skincolorwhite  = false
+var Married     = false
+var single  = true
+var beerlover = true
+
+// NULL 
+var password =""
+var work =""
+var chuch =""
+var house =""
+var justme =""
+
+// UNDEFINED 
+var FutureMe
+var kids
+var wife
+var bigbro
+var nothing
